496991799cc02c72147b0970211eaaede7a4c53a
K1lla001 <97819936+K1lla001@users.noreply.github.com>
2022-12-09 16:01:18 +0700

    Create codeSoalNo1.cpp

---
+++ codeSoalNo1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	  char Karakter = 'A';
+	  string Universit4s = "UNSIA"; 
+	  string Jurusan = "Informatika";
+	  string firstName = "Budi";
+	  cout << Karakter << endl;
+	  cout << Universit4s << endl;
+	  cout << Jurusan << endl;
+	  cout << firstName << endl;
+	  return 0;
+}
